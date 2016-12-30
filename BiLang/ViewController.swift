bb74d57900500069931a7739ce50ef788e1b2e01
최광훈 <hoonhoon@woowahan.com>
2016-12-30 19:31:37 +0900

    Initial Commit

---
+++ BiLang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BiLang
+//
+//  Created by hoonhoon on 2016. 12. 30..
+//  Copyright © 2016년 최광훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
